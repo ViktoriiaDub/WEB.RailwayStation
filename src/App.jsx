7a760bc24db2b579daf0abe97afb4aa0a22b46f6
ViktoriiaDub <dybviktoria@gmail.com>
2026-05-28 11:59:24 +0300

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BookingProvider } from './context/BookingContext'
 import Home from './pages/Home'
+import Booking from './pages/Booking'
 import './App.css'
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <BookingProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/booking/:trainId" element={<Booking />} />
+        </Routes>
+      </BookingProvider>
     </BrowserRouter>
   )
 }
